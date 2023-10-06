73bc33f6bd417686ec667f27a222ee77bbcd1502
Josh Owens <josh.owens.pro@gmail.com>
2023-10-06 12:00:07 -0400

    fixed padding for mobile

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -5,6 +5,12 @@ const Layout = styled(Box)`
   margin-right: auto;
   max-width: 1280px;
   padding: 0 80px;
+  ${(props) => props.theme.breakpoints.down("md")} {
+    padding: 0 48px;
+  }
+  ${(props) => props.theme.breakpoints.down("sm")} {
+    padding: 0 24px;
+  }
 `;
 
 export default Layout;
